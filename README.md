842efd968d5c6bb469c60264969b00158aa95c16
Jacob Markovich <markovich.jacob@gmail.com>
2020-06-19 00:17:09 -0500

    First draft?
    Hopefully this is also the last draft...

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+
+# Languages used:  HTML, CSS, and JavaScript
+
+This project took forever for me and I felt I was behind the curve most of the way through it.  However, I did manage to get a working page that I believe meets all expectations.  Links were created via JavaScript in an unordered list, the links scroll slowly to the intended targets, the active class is attached to each section as it enters the viewport, and finally the sections that aren't active have the active class removed.
--- js/app.js
+++ js/app.js
@@ -11,63 +11,84 @@
  *
  * JS Standard: ESlint
  *
-*/
+ */
 
 /**
  * Define Global Variables
  *
-*/
+ */
 // Stores the unordered list of links
-const navList = document.getElementById('navbar__list');
+const navList = document.getElementById("navbar__list");
 // captures the sections to be used to create the navBar links
-const sections = document.querySelectorAll('section');
-
+const sections = document.querySelectorAll("section");
 /**
  * End Global Variables
  * Start Helper Functions
  *
-*/
-function scrollTo(target) {
-  $('html, body').animate({
-    scrollTop: target ? target.offset().top : 0
-  },'slow');
-}
-
-
+ */
+// adds a listener to the links in which it prevents the html from scrolling so it can make it smooth.
+const scroll = () => {
+    let navLinks = document.getElementsByTagName("a");
+    for (i = 0; i < navLinks.length; i++) {
+        navLinks[i].addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetID = event.currentTarget.getAttribute("href");
+            window.scrollTo({
+                top: document.querySelector(targetID).offsetTop,
+                behavior: "smooth",
+            });
+        });
+    }
+};
+// checks which section is in the viewport
+const activateElem = () => {
+    maxSection = sections[0];
+    minVal = 1000000;
+    for (item of sections) {
+        let padding = item.getBoundingClientRect();
+        if ((padding.top > -400) & (padding.top < minVal)) {
+            minVal = padding.top;
+            maxSection = item;
+        }
+    }
+    return maxSection;
+};
 /**
  * End Helper Functions
  * Begin Main Functions
  *
-*/
-
+ */
 // build the navigation bar, loop over the sections and create an unordered list and links
 const navBar = () => {
-  let navUI = '';
-  navUI.onclick = () => scrollTo();
-  sections.forEach(section => {
-    const sectionNumber = section.id;
-    const sectionName = section.dataset.nav;
-    navUI += `<li><a class="menu__link" href="#${sectionNumber}">${sectionName}</a></li>`;
-    navList.onclick = () => scrollTo($(`#${section.id}`));
-  });
-  // appends the newly built unordered list and links to the site
-  navList.innerHTML = navUI;
-}
-// runs the newly built navBar function which creates the navigation bar
-navBar();
+    let navUI = "";
+    sections.forEach((section) => {
+        const sectionNumber = section.id;
+        const sectionName = section.dataset.nav;
+        navUI += `<li><a class="menu__link" href="#${sectionNumber}">${sectionName}</a></li>`;
+    });
+    navList.innerHTML = navUI;
+};
 
-// Add class 'active' to section when near top of viewport
-
-
-
+// Add class 'active' to section when near top of viewport and removes that class when not in the viewport
+const addActive = () => {
+    window.addEventListener("scroll", () => {
+        let section = activateElem();
+        section.classList.add("your-active-class");
+        for (let item of sections) {
+            if ((item.id != section.id) & item.classList.contains("your-active-class")) {
+                item.classList.remove("your-active-class");
+            }
+        }
+    });
+};
 /**
  * End Main Functions
  * Begin Events
  *
-*/
-
+ */
 // Build menu
-
+navBar();
 // Scroll to section on link click
-
+scroll();
 // Set sections as active
+addActive();
